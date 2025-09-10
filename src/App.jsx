b3fbe0c76149becc1c29f3b9bc11afaa73c3d03d
Destiny Oladapo Adu <aduoladapo9@gmail.com>
2025-09-10 15:06:21 +0100

    First Commit.

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+const App = () => {
+  return (
+    <div className="login-container">
+      <h2 className="form-title">Log in with</h2>
+      <div className="social-login">
+        <button className="social-btn">
+          <img src="google.svg" alt="Google" className="social-icon" />
+          Google
+        </button>
+
+        <button className="social-btn">
+          <img src="apple.svg" alt="Apple" className="social-icon" />
+          Apple
+        </button>
+      </div>
+
+      <p className="separator"><span>or</span></p>
+
+      <form action="#" className="login-form">
+        <div className="input-wrapper">
+          <input type="email" placeholder="Email address" 
+          className="input-field" required/>
+          <i className="material-symbols-rounded">mail</i>
+        </div>
+
+
+        <div className="input-wrapper">
+          <input type="password" placeholder="Password" 
+          className="input-field" required/>
+          <i class="material-symbols-rounded">lock</i>
+        </div>
+
+        <a href="#" className="forgot-pass-link">Forgot Password?</a>
+
+        <button className="login-btn">Log In</button>
+
+      </form>
+
+      <p className="signup-text">Don't have an account? <a href="#">Sign Up</a></p>
+
+    </div>
+  )
+}
+
+export default App
